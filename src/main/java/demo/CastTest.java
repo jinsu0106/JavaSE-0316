61deefc8c9e0a91bbe3fc7173d65a641a414f1a0
thu <mingfei.net@gmail.com>
2019-03-15 16:54:16 +0800

    Update.

---
+++ src/main/java/demo/CastTest.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class CastTest {
+    public static void main(String[] args) {
+
+        // char [0, 65535]
+        // int [-2147483648, 2147483647]
+        char c = 'a'; // 97
+        int i = c;
+
+        System.out.println(i);
+
+        int x = 2147483647;
+        char y = (char)x;
+
+        System.out.println(y);
+
+
+    }
+}
---
+++ src/main/java/demo/LogicalTest.java
@@ -0,0 +1,43 @@
+package demo;
+
+public class LogicalTest {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = a & b;
+        System.out.println(c);
+
+        c = a | b;
+        System.out.println(c);
+
+        c = a ^ b; // 异
+        System.out.println(c);
+
+        c = !a;
+        System.out.println(c);
+
+
+        c = a && b;
+        System.out.println(c);
+
+        c = a || b;
+        System.out.println(c);
+
+        int x = 0;
+        int y = 1;
+
+        boolean z = a || (x++ > y);
+        System.out.println(z);
+        System.out.println(x); // ?
+
+
+//        x += y 等价于 x = x + y;
+
+        int n = (++x == y) ? x++ : --y;
+
+        System.out.println(n);
+        System.out.println(x);
+    }
+}

0e9b60563a3292e9fe364728ab4b9bbdba484650
thu <mingfei.net@gmail.com>
2019-03-15 14:49:42 +0800

    Update.

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -2,6 +2,35 @@ package demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Java SE!");
+
+        int x = 0;
+
+        long l1 = 2L; // Java int
+
+        float f = 1.2f;
+
+        int y = 0b101010;
+        int m = 07654321;
+        int n = 0xabcdef;
+
+        char c = '\u9fbb'; // '\u4e00'; // '\123'; // 97; // 'a'; // 1;
+
+        System.out.println((int)'一');
+
+        if (x == 1) {
+            System.out.println("Hello, Java SE!");
+        }
+
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
+        System.out.println(s1);
+        System.out.println(s3);
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+
+        System.out.println(s1.equals(s3));
+
     }
 }

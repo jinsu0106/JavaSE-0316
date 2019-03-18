ac585d17286643135d38fb2cc267dad03c991866
thu <mingfei.net@gmail.com>
2019-03-18 16:56:39 +0800

    Update.

--- src/main/java/day02/PassTest.java
+++ src/main/java/day02/PassTest.java
@@ -2,16 +2,32 @@ package day02;
 
 public class PassTest {
 
-    private static void change(int i) {
+    private static void change(int i) { // 基本数据类型
         System.out.println(i); // 0
         i++;
         System.out.println(i); // 1
     }
 
-    public static void main(String[] args) {
-        int i = 0;
-        System.out.println(i); // 0
-        change(i);
-        System.out.println(i); // ?
+    private static void change(Parameter parameter) { // 引用数据类型
+        System.out.println(parameter.anInt); // 2: 0
+        parameter.anInt ++;
+        System.out.println(parameter.anInt); // 3: 1
     }
+
+    public static void main(String[] args) {
+//        int i = 0;
+//        System.out.println(i); // 0
+//        change(i);
+//        System.out.println(i); // 0
+
+        Parameter parameter = new Parameter();
+        System.out.println(parameter.anInt); // 1: 0
+        change(parameter);
+        System.out.println(parameter.anInt); // 4: 1
+    }
+}
+
+
+class Parameter {
+    int anInt;
 }

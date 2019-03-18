480a58bc69705dcc79de2e3032e80f4cd6959d8e
thu <mingfei.net@gmail.com>
2019-03-18 11:14:39 +0800

    Update.

--- src/main/java/day02/AbstractTest.java
+++ src/main/java/day02/AbstractTest.java
@@ -1,5 +1,7 @@
 package day02;
 
+import javafx.scene.shape.Circle;
+
 /**
  * abstract: class method
  */
@@ -10,19 +12,39 @@ public class AbstractTest {
         Circle circle = new Circle();
 
 //        circle.method();
+
+        Rectangle rectangle = new Rectangle(2, 3);
+        System.out.println(rectangle.area());
+        System.out.println(rectangle.perimeter());
     }
 }
 
 abstract class Shape {
-    public abstract void area();
-    // ...
+    public abstract double area();
+
+    public abstract double perimeter();
 }
 
 // Circle Square Rectangle Triangle: area ...
-class Circle extends Shape {
+
+class Rectangle extends Shape {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
 
     @Override
-    public void area() {
+    public double area() {
+        return width * height;
+    }
 
+    @Override
+    public double perimeter() {
+        return (width + height) * 2;
     }
 }
+

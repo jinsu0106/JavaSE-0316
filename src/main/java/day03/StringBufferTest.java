00ff72214863c94b2e23f74a080ce5594fe9900b
thu <mingfei.net@gmail.com>
2019-03-19 09:14:05 +0800

    Update.

--- src/main/java/day03/StringBufferTest.java
+++ src/main/java/day03/StringBufferTest.java
@@ -3,9 +3,17 @@ package day03;
 public class StringBufferTest {
 
     public static void main(String[] args) {
-        StringBuffer stringBuffer = new StringBuffer(); //
+
+        String s = "hello";
+        StringBuffer stringBuffer = new StringBuffer(s); //
+
+        String string = stringBuffer.toString();
 
         // String -> StringBuffer
         // StringBuffer -> String
+
+        
+        // String 不可变的 降低运行速度
+        // StringBuffer 可变的
     }
 }

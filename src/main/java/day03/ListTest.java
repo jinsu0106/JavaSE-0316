728ee2beba859a8b20a9eb22b21dc01840a9d47a
thu <mingfei.net@gmail.com>
2019-03-19 14:52:41 +0800

    Update.

--- src/main/java/day03/ListTest.java
+++ src/main/java/day03/ListTest.java
@@ -7,5 +7,24 @@ public class ListTest {
     public static void main(String[] args) {
         // Shift + Ctrl + Alt + U
 //        ArrayList
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("hello");
+        strings.add("hi");
+
+        System.out.println(strings.size());
+
+        System.out.println(strings.get(0));
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        // 获取 ArrayList 的当前容量？
+//        System.out.println(strings.capacity()); //
+
+        Vector<Integer> integers = new Vector<>();
+        System.out.println(integers.capacity());
     }
 }

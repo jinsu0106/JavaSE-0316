27e2c8401cb14034dc3dd1f32520dff812ff6262
thu <mingfei.net@gmail.com>
2019-03-19 09:03:47 +0800

    Update.

---
+++ src/main/java/day03/StringBufferTest.java
@@ -0,0 +1,11 @@
+package day03;
+
+public class StringBufferTest {
+
+    public static void main(String[] args) {
+        StringBuffer stringBuffer = new StringBuffer(); //
+
+        // String -> StringBuffer
+        // StringBuffer -> String
+    }
+}
---
+++ src/main/java/day03/StringTest.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s);
+        
+        s = "hi";
+        System.out.println(s); // ?
+
+        // 字符串初始化后，其值不能改变 immutable
+
+        // GC
+    }
+}

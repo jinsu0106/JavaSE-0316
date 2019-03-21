48a8eef655d110c5e74b86253f2494050b36a0fb
thu <mingfei.net@gmail.com>
2019-03-21 09:31:19 +0800

    Update.

--- src/main/java/day05/InputStreamTest.java
+++ src/main/java/day05/InputStreamTest.java
@@ -4,12 +4,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ *  输入流
+ *  Reader -> FileReader -> method
+ */
 public class InputStreamTest {
+
+    private static final String NAME = "src/main/java/day05/InputStreamTest.java";
 
     public static void main(String[] args) {
         try {
-            InputStream inputStream = new FileInputStream("build.gradle");
-            System.out.println((char)inputStream.read());
+            InputStream inputStream = new FileInputStream(NAME);
+            int i = inputStream.read();
+            while (i != -1) {
+                System.out.print((char) i);
+                i = inputStream.read();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }

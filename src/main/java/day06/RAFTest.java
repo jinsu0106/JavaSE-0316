557fe067a2727e1f8c6ebcf008904ee1cf24082e
thu <mingfei.net@gmail.com>
2019-03-22 09:20:59 +0800

    Update.

--- src/main/java/day06/RAFTest.java
+++ src/main/java/day06/RAFTest.java
@@ -1,21 +1,34 @@
 package day06;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 
 /**
  * DataInputStream
  * DataOutputStream
  */
 public class RAFTest {
-    public static void main(String[] args) {
+    public static void raf() {
         try (RandomAccessFile randomAccessFile = new RandomAccessFile("raf", "rw")) {
             for (int i = 0; i < 10; i++) {
                 randomAccessFile.writeDouble(i * 0.5);
             }
             randomAccessFile.seek(8);
             System.out.println(randomAccessFile.readDouble()); // EOF: End Of File
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (
+                DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("io"));
+                DataInputStream dataInputStream = new DataInputStream(new FileInputStream("io"))
+        ) {
+            for (int i = 0; i < 10; i++) {
+                dataOutputStream.writeDouble(i * 0.5);
+            }
+            dataInputStream.skipBytes(8);
+            System.out.println(dataInputStream.readDouble());
         } catch (IOException e) {
             e.printStackTrace();
         }
